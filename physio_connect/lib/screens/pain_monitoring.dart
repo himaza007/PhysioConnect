f5f92afa05e2945a70bcac2b8ead2f237dd01846
Sachira Delankawala <sachirad24@gmail.com>
2025-03-03 01:35:36 +0530

    Update pain_monitoring.dart

--- physio_connect/lib/screens/pain_monitoring.dart
+++ physio_connect/lib/screens/pain_monitoring.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 import '../utils/storage_helper.dart';
+
+class PainMonitoringPage extends StatefulWidget {
+
+  @override
+  _PainMonitoringPageState createState() => _PainMonitoringPageState();
+}
+
+class _PainMonitoringPageState extends State<PainMonitoringPage> {
+  int _painLevel = 5;
+  String _painLocation = '';
+  List<Map<String, dynamic>> _painHistory = [];
