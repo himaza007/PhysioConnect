373497200e59c6334fe64597e3971eaa0f6a5164
Sathnara3 <sathnaraperera3@gmail.com>
2025-03-05 11:57:12 +0530

    Updated some parts

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'educational_resources.dart'; // Import the new screen
 import 'customized_injury_remedies.dart'; // Import the new screen
 
 void main() {
