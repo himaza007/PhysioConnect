9ea3b5affcde3441307f9b1100a09d00e2a9a665
Sachira Delankawala <sachirad24@gmail.com>
2025-03-05 16:14:56 +0530

    Update storage_helper.dart

--- physio_connect/lib/utils/storage_helper.dart
+++ physio_connect/lib/utils/storage_helper.dart
@@ -3,3 +3,9 @@ import 'dart:convert';
 
 class StorageHelper {
   static Future<List<Map<String, dynamic>>> loadPainHistory() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? storedData = prefs.getString('painHistory');
+    return storedData != null
+        ? List<Map<String, dynamic>>.from(json.decode(storedData))
+        : [];
+  }
