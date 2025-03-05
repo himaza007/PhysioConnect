f13676a77f3a0268a3dbac7e469eec379f940256
Azeezur-r <azeezrahmaan.azwin@gmail.com>
2025-03-05 15:59:32 +0530

    create message screen

---
+++ messaging_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MessagingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Support & Chat'),
+        backgroundColor: Colors.green.shade700,
+      ),
+      body: Center(child: Text('Messaging Feature Coming Soon...')),
+    );
+  }
+}
