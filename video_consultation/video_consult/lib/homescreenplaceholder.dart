01c9c0aab154bf2c6b367d957649e9ff606cb72a
Azeezur-r <azeezrahmaan.azwin@gmail.com>
2025-03-05 19:57:36 +0530

    create homescreenplaceholder

---
+++ video_consultation/video_consult/lib/homescreenplaceholder.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.green.shade700,
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Home Screen',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
