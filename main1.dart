ecd52609b32b824d2a805171db994c528d2a3eb7
THEJAN04 <pthejan2004@gmail.com>
2025-03-06 07:27:56 +0530

    main dart

---
+++ main1.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'custom_exercise_plans.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Physio Educational Resources',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF33724B), // Midnight Teal
+        scaffoldBackgroundColor: const Color(0xFFEAF7FF), // Alice Blue
+      ),
+      home: const CustomExercisePlansScreen(), // Automatically loads this page
+    );
+  }
+}
