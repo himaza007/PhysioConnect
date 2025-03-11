3f691f510722a8b1a321031c05c029a070df795c
Saad Mazhar <92235123+saadmazhar03@users.noreply.github.com>
2025-03-11 09:25:06 +0530

    Added Patient Name Parameter
    Modified PatientDetailsScreen to accept patientName as a required parameter.

--- ephr/lib/patient_detail_screen.dart
+++ ephr/lib/patient_detail_screen.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 class PatientDetailsScreen extends StatelessWidget {
+  final String patientName; // Added parameter to accept patient name
+
+  PatientDetailsScreen({required this.patientName});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Patient Details"),
-        backgroundColor: Colors.green[700], // Set app bar color
+        title: Text(patientName), // Display patient's name in the AppBar
+        backgroundColor: Colors.green[700],
       ),
       body: Center(
         child: Text(
-          "Patient details will be displayed here.",
-        ), // Placeholder text
+          "Patient Name: $patientName", // Display patient name in body
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
