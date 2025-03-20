6bd1e57fcf5bcfc2e0373066f9495a46512e4a3b
Sathnara3 <sathnaraperera3@gmail.com>
2025-03-20 23:44:00 +0530

    connecting

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,16 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/injury_remedies', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
 // Seed Database
 const seedDatabase = async () => {
   await KTaping.deleteMany({});
