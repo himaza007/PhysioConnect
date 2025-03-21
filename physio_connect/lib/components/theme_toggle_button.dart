831b5fa03281c418030c0f5b50ca68e737cd4085
Sachira Delankawala <sachirad24@gmail.com>
2025-03-21 23:48:29 +0530

    adjusting components

--- physio_connect/lib/components/theme_toggle_button.dart
+++ physio_connect/lib/components/theme_toggle_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../components/theme_provider.dart'; 
+import '../components/theme_provider.dart';
 
 class ThemeToggleButton extends StatelessWidget {
   @override
