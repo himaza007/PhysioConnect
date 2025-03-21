0259b3d6b1c98587fa208e13a87cc5d124216fc8
Sathnara3 <sathnaraperera3@gmail.com>
2025-03-21 18:23:29 +0530

    Created resource.js

---
+++ backend/resource.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const subtopicSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  icon: { type: String, required: true },
+});
