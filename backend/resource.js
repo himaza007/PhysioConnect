933475fb1caffcaa5fe43b1ed5ab4af6914764d1
Sathnara3 <sathnaraperera3@gmail.com>
2025-03-21 18:29:08 +0530

    pt2 resource.js

--- backend/resource.js
+++ backend/resource.js
@@ -6,3 +6,11 @@ const subtopicSchema = new mongoose.Schema({
   description: { type: String, required: true },
   icon: { type: String, required: true },
 });
+
+const resourceSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    subtopics: [subtopicSchema],
+  });
+  
+  module.exports = mongoose.model("Resource", resourceSchema);
