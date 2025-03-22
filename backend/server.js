26f28d5d374372320457991bbc2f08c0ae5cbb7a
Sathnara3 <sathnaraperera3@gmail.com>
2025-03-22 11:38:50 +0530

    Middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-require
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const resourceRoutes = require("./routes/resources");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("/api/resoureces", resourceRoutes);
+
